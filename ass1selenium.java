959548fca859bf6db5802824eb4d5b1f861f1066
TArjun00 <107174241+TArjun00@users.noreply.github.com>
2022-06-11 20:01:25 +0530

    Add files via upload

---
+++ ass1selenium.java
@@ -0,0 +1,14 @@
+package seleniumass;
+
+import org.openqa.selenium.WebDriver;
+
+public class ass1selenium {
+
+	public static void main(String[] args) {}
+	
+	private static void firstautomation() {
+		
+		System.out.println("finally added a file");
+	}
+
+}
